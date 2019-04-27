091fe9b626a7e2cdbbcdd7d9070f8ae30dd6e4c7
kevinator7000 <kevinator7000@users.noreply.github.com>
2019-04-27 11:59:29 -0700

    modify

---
+++ Ti-poll/Ti-poll/EditSurvey.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ti_poll
+{
+    /// <summary>
+    /// Interaction logic for EditSurvey.xaml
+    /// </summary>
+    public partial class EditSurvey : Window
+    {
+        public Clases.Survey survey;
+
+        public EditSurvey(Clases.Survey survey)
+        {
+            InitializeComponent();
+            this.survey = survey;
+            questions_lb.ItemsSource = survey.profiles;
+        }
+        
+    }
+}
--- Ti-poll/Ti-poll/Home.xaml.cs
+++ Ti-poll/Ti-poll/Home.xaml.cs
@@ -23,6 +23,7 @@ namespace Ti_poll
         public Home()
         {
             InitializeComponent();
+            
         }
 
         private void searchbar_KeyDown(object sender, KeyEventArgs e)
@@ -49,15 +50,6 @@ namespace Ti_poll
             Hide();
         }
 
-
-        private void Add_Survey_Click(object sender, RoutedEventArgs e)
-        {
-            AddSurvey a = new AddSurvey();
-            a.Owner = this;
-            a.Show();
-            Hide();
-        }
-
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Owner.Show();
@@ -66,6 +58,45 @@ namespace Ti_poll
         private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             profile_button.Content = Database.CurrentUser.Name;
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AddSurvey a = new AddSurvey();
+            a.Owner = this;
+            a.Show();
+            Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (int i in Database.CurrentUser.Surveys)
+            {
+                Clases.Survey s = Database.data.GetSurvey(i);
+                Label l = new Label();
+                l.Content = s.Name + " " + s.Category + " " + s.Views;
+                l.Tag = i;
+                panel.Children.Add(l);
+                l.MouseDown += L_MouseDown;
+            }
+        }
+
+        private void L_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Clases.Survey s;
+            Label l = sender as Label;
+            int i = (int)l.Tag;
+            for (int j = 0; j < Database.CurrentUser.Surveys.Count; j++)
+            {
+                if (j==i)
+                {
+                    s = Database.data.GetSurvey(j);
+                    EditSurvey es = new EditSurvey(s);
+                    es.Owner = this;
+                    es.Show();
+                    Hide();
+                }
+            }
         }
     }
 }
