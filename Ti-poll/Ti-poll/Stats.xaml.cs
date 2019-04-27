a9c90caf3a860a37728abba909d6728a2f931f20
jorgemejiaa <49921819+jorgemejiaa@users.noreply.github.com>
2019-04-27 11:34:15 -0700

    Trabajando en stadisticas

--- Ti-poll/Ti-poll/Stats.xaml.cs
+++ Ti-poll/Ti-poll/Stats.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
@@ -13,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Ti_poll.Clases;
 
 namespace Ti_poll
 {
@@ -21,45 +21,45 @@ namespace Ti_poll
     /// </summary>
     public partial class Stats : Window
     {
-        //public Timer timer;
+        public Timer timer;
         public Stats()
         {
             InitializeComponent();
+            //questions_lb.Items.Add("hola");
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Clases.Survey sac = new Clases.Survey();
 
             if (combo_cb.SelectedIndex == 0)
             {
-                int hombres = 10;
-                int mujeres = 10;
+                h_pb.Maximum = sac.ViewsH;
+                m_pb.Maximum = sac.ViewsM;
+                x_pb.Maximum = sac.ViewsH + sac.ViewsM;
 
-                h_pb.Maximum = hombres;
-                m_pb.Maximum = mujeres;
-                x_pb.Maximum = hombres + mujeres;
-
-                //timer = new Timer(1);
-                //timer.Elapsed += Timer_Elapsed;
-                //timer.Enabled = true;
+                timer = new Timer(1);
+                timer.Elapsed += Timer_Elapsed;
+                timer.Enabled = true;
 
             }
         }
 
-
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
+            Clases.Survey sac = new Clases.Survey();
+            Profile pro = new Profile();
             Dispatcher.Invoke((Action)(() =>
             {
-                //if (h_pb.Value < siH) h_pb.Value += 1;
+                if (h_pb.Value < sac.ViewsH) h_pb.Value += 1;
 
-                //if (m_pb.Value < siM) m_pb.Value += 1;
+                if (m_pb.Value < sac.ViewsM) m_pb.Value += 1;
 
-                //if (x_pb.Value < siH + siM) x_pb.Value += 1;
-                //if (x_pb.Value == siH + siM && h_pb.Value == siH && m_pb.Value == siM)
-                //{
-                //    timer.Dispose();
-                //}
+                if (x_pb.Value < sac.ViewsM + sac.ViewsH) x_pb.Value += 1;
+                if (x_pb.Value == sac.ViewsH+sac.ViewsM && h_pb.Value == sac.ViewsH && m_pb.Value == sac.ViewsM)
+                {
+                    timer.Dispose();
+                }
 
             }));
         }
