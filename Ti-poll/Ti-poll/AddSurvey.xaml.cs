b82ae8cea537b1503f934d1558f63fab5296f064
kevinator7000 <kevinator7000@users.noreply.github.com>
2019-04-26 20:10:17 -0700

    add question/ survey

---
+++ Ti-poll/Ti-poll/AddSurvey.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ti_poll
+{
+    /// <summary>
+    /// Interaction logic for AddSurvey.xaml
+    /// </summary>
+    public partial class AddSurvey : Window
+    {
+        public AddSurvey()
+        {
+            InitializeComponent();
+        }
+    }
+}
