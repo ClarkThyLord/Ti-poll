8c80825e58d59392842cafd8b0a804c6cbaa2368
Christian Cuevas <ccpotential@gmail.com>
2019-04-26 21:25:38 -0700

    SplashScreen
    Clark was here :V

---
+++ Ti-poll/Ti-poll/SplashScreen.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ti_poll
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SpalshScreen : Window
+    {
+        public Timer timer;
+        public event EventHandler Finished;
+
+        public SpalshScreen()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            timer = new Timer(10);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke((Action)(() =>
+            {
+                if (progress.Value < 100) progress.Value += 1;
+                else
+                {
+                    timer.Enabled = false;
+                    Finished(this, EventArgs.Empty);
+                }
+            }));
+        }
+    }
+}
